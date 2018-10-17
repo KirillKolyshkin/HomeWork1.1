6b689068dc5b5c5cda47a31f6eb244408620a932
KillReal <kolyshkin.kirill@yandex.ru>
2018-10-18 02:08:53 +0300

    now work in background

---
+++ app/src/main/java/com/example/kolys/homework2/TimeNotification.java
@@ -0,0 +1,47 @@
+package com.example.kolys.homework2;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
+
+public class TimeNotification extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("best channel", "My channel",
+                    NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("My channel description");
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+            channel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION), channel.getAudioAttributes());
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(context, "best channel")
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("Notification")
+                        .setContentText("Wake up, Grab a brash and put a little makeup!");
+        Intent intentTL = new Intent(context, WakeUpActivity.class);
+        intentTL.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pIntent = PendingIntent.getActivity(
+                context,
+                0,
+                intentTL,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+        builder.setContentIntent(pIntent);
+        Notification notification = builder.build();
+        notificationManager.notify(1, notification);
+    }
+
+}
